1b0497dabc5def418cdff66d3c923037e657c749
Roshan Timalsina <timalsinaroshan345@gmail.com>
2026-05-24 21:07:11 +0545

    update

--- sign-in.php
+++ sign-in.php
@@ -14,8 +14,8 @@ $error = isset($_GET['error']) ? htmlspecialchars($_GET['error']) : '';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign In - <?= htmlspecialchars($config['app_name']) ?></title>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=2">
+    <link rel="stylesheet" href="assets/css/responsive.css?v=2">
 </head>
 <body class="auth-page">
     <div class="auth-card">
