8dd551162a6d1067221a661d0729d1e320eac00e
Roshan Timalsina <timalsinaroshan345@gmail.com>
2026-05-27 22:08:31 +0545

    errorfixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const elements = document.querySelectorAll(".scroll-reveal");
 
+  elements.forEach((el) => {
+    el.classList.add("visible");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".scroll-reveal");
+
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
